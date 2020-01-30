f4248d7390c1bb7576e1d7d84ce1d2fb71788913
Matheus Kindrazki <matheus@mentores.com.br>
2020-01-29 21:49:42 -0300

    criando test de exemplo

---
+++ __tests__/example.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+  public function testeCanBeMessage(): void
+  {
+        $this->assertEquals(
+            'testes',
+            'testes'
+        );
+    }
+}
+
+
