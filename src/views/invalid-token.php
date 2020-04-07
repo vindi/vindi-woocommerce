9fd9e0346da50132e87edeab422aca277369929e
Thiago Henrique Xavier Medeiros <thiago@mentores.com.br>
2020-04-07 16:34:33 -0300

    fechar notificação de token inválido

--- src/views/invalid-token.php
+++ src/views/invalid-token.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="error">
+<div class="notice notice-error is-dismissible">
 	<p><strong><?php _e( 'Invalid Token', 'vindi-woocommerce' ); ?></strong>: <?php printf( __( 'The provided API token is not a valid Vindi API token! Please check your spelling and try again!', 'vindi-woocommerce' ) ); ?></p>
 </div>
