c963ae25b6daf758bbaed57df8dd826d135c6b16
Thiago Medeiros <thiago@mentores.com.br>
2020-04-23 14:11:14 -0300

    fix(bankslippayment.php): checagem se a order não foi cancelada
    O aviso para baixar o boleto continuava aparecendo se a order fosse cancelada. Para evitar isso, foi
    adicionada uma checagem para verificar se o status dela não contém o status cancelado

--- src/includes/gateways/BankSlipPayment.php
+++ src/includes/gateways/BankSlipPayment.php
@@ -117,7 +117,7 @@ class VindiBankSlipGateway extends VindiPaymentGateway
   public function show_bank_slip_download($order_id) {
     $order = wc_get_order($order_id);
     if ($download_url = get_post_meta($order_id, 'vindi_wc_bank_slip_download_url', true)) {
-      if(!$order->is_paid()) {
+      if(!$order->is_paid() && !$order->has_status('cancelled')) {
         $this->vindi_settings->get_template('bankslip-download.html.php', compact('download_url'));
       }
     }
