4ad54d4e6d89d51707bf36c59d887a3c38debbba
Wellington Trojan <wellington@mentores.com.br>
2020-06-05 15:27:03 -0300

    feat(product-status.php): alertas style
    CSS para caixas de alerta do status de produto

--- src/views/invalid-api-key.php
+++ src/views/invalid-api-key.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="notice notice-error is-dismissible">
+<div class="vindi-alert notice vindi-warning">
 	<p><strong><?php _e( 'Chave API Inválida', VINDI ); ?></strong>: <?php printf( __( 'A chave API utilizada não é uma chave válida! Por favor verifique as informações e tente novamente!', VINDI ) ); ?></p>
 </div>
--- src/views/product-status.php
+++ src/views/product-status.php
@@ -11,16 +11,16 @@ $type = get_transient('vindi_product_message');
 ?>
 
 <?php if ($type == 'created'): ?>
-  <div class="notice notice-success is-dismissible">
+  <div class="vindi-alert notice vindi-success">
 		<p><strong><?php _e('Vindi', VINDI);?></strong>: <?php printf( __( 'O produto foi criado na Vindi com sucesso!', VINDI ) ); ?></p>
 	</div>
 <?php elseif ($type == 'updated'): ?>
-  <div class="notice notice-success is-dismissible">
+  <div class="vindi-alert notice vindi-success">
 		<p><strong><?php _e('Vindi', VINDI);?></strong>: <?php printf( __( 'O produto foi atualizado na Vindi com sucesso!', VINDI ) ); ?></p>
 	</div>
 <?php else: ?>
-  <div class="notice notice-error is-dismissible">
-		<p><strong><?php _e('ERRO', VINDI);?></strong>: <?php printf( __( 'Não foi possível criar/atualizar o produto na Vindi!', VINDI ) ); ?></p>
+  <div class="vindi-alert notice vindi-error">
+		<p><strong><?php _e('Vindi', VINDI);?></strong>: <?php printf( __( 'Não foi possível criar/atualizar o produto na Vindi!', VINDI ) ); ?></p>
 	</div>
 <?php
 	endif;
