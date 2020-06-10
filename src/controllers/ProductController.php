922bf523707ee036e98832f0476cadf75c91aa83
Thiago Medeiros <thiago@mentores.com.br>
2020-06-10 15:18:30 -0300

    fix: correção de loop infinito se produto/plano nao fosse criado

--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -85,11 +85,10 @@ class ProductController
     ));
 
     // Saving product id and plan in the WC goal
-    update_post_meta( $post_id, 'vindi_product_id', $createdProduct['id'] );
-
-    update_post_meta( $post_id, 'vindi_product_created', true );
-
     if($createdProduct) {
+      update_post_meta( $post_id, 'vindi_product_id', $createdProduct['id'] );
+      update_post_meta( $post_id, 'vindi_product_created', true );
+
       set_transient('vindi_product_message', 'created', 60);
     } else {
       set_transient('vindi_product_message', 'error', 60);
