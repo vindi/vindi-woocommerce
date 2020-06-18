4d0b1ec68c777e0053fe1027fb5a681f38173cd3
Thiago Medeiros <thiago@mentores.com.br>
2020-06-18 09:30:57 -0300

    test(test-payment-processor.php): teste do processador de pagamentos
    Teste funcionando. Ele verifica se o retorno do processamento de uma order está condizendo com as
    informações que devem ser mandadas para API

--- tests/phpunit/vindi/base/test-payment-processor.php
+++ tests/phpunit/vindi/base/test-payment-processor.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once VINDI_PATH . 'src/includes/admin/Settings.php';
+include_once VINDI_PATH . 'src/routes/RoutesApi.php';
 include_once VINDI_PATH . 'src/controllers/index.php';
 include_once VINDI_PATH . 'src/utils/PaymentProcessor.php';
 include_once VINDI_PATH . 'src/utils/PaymentGateway.php';
@@ -9,9 +10,49 @@ include_once VINDI_PATH . 'src/includes/gateways/CreditPayment.php';
 
 class Vindi_Test_Payment_Processor extends Vindi_Test_Base
 {
+  private $mock_shipping_product;
+  private $customer;
+
+  public function setUp() {
+    $this->mock_shipping_product = array(
+      'id' => 465,
+      'name' => 'Frete (Flat rate)',
+      'code' => 'flat-rate',
+      'unit' => '',
+      'status' => 'active',
+      'description' => '',
+      'invoice' => 'always',
+      'created_at' => '2020-06-08T11:20:57.000-03:00',
+      'updated_at' => '2020-06-08T11:20:57.000-03:00',
+      'pricing_schema' => array(
+        'id' => 'string',
+        'short_format' => 'R$ 10,0',
+        'price' => 10.0,
+        'minimum_price' => null,
+        'schema_type' => 'flat',
+        'pricing_ranges' => array(),
+        'created_at' => '2020-06-08T11:20:57.000-03:00'
+      ),
+      'metadata' => array()
+    );
+    $this->customer = WC_Helper_Customer::create_customer();
+    update_user_meta($this->customer->get_id(), 'vindi_customer_id', 328876);
+  }
+
+  public function tearDown()
+  {
+    $this->mock_shipping_product = null;
+    wp_delete_user($this->customer->get_id());
+    wc_delete_user_data($this->customer->get_id());
+    $this->customer = null;
+  }
+
   public function test_processor_build_items()
   {
+    $routes = $this->createMock(VindiRoutes::class);
+    $routes->method('findOrCreateProduct')->willReturn($this->mock_shipping_product);
     $settings = new VindiSettings();
+    $settings->routes = $routes;
     $controllers = new VindiControllers($settings);
 
     $credit_card = new VindiCreditGateway($settings, $controllers);
@@ -23,48 +64,51 @@ class Vindi_Test_Payment_Processor extends Vindi_Test_Base
       return;
     }
 
-    //   // Arrange: Create a couple of products to use.
-    //   // $simple_product = WC_Helper_Product::create_simple_product();
+    // Arrange: Create a couple of products to use.
+    // $simple_product = WC_Helper_Product::create_simple_product();
 
-    //   $product = new WC_Product();
+    $product = new WC_Product();
 
-    //   $product->set_name('teste 123');
+    $product->set_name('teste 123');
 
-    //   $product->set_price(10);
+    $product->set_price(20);
+    $product->save();
 
-    //   update_post_meta($product->get_id(), 'vindi_product_id', 1239812039);
+    update_post_meta($product->get_id(), 'vindi_product_id', 1239812039);
 
-    // // Arrange: Set up an order
-    // $order = WC_Helper_Order::create_order();
+    // Arrange: Set up an order
+    $order = WC_Helper_Order::create_order($this->customer->get_id());
+    $order->remove_order_items('line_item');
 
-    // $order->add_product($product_1, 1); 
+    $order->add_product($product, 4, array(
+      'subtotal' => 80,
+      'total' => 80
+    ));
 
-    // $order->set_payment_method($credit_card->id);
-    // $order->set_payment_method_title($credit_card->method_title);
-    // $order->save();
-    // $order->calculate_totals();
+    $order->set_payment_method($credit_card->id);
+    $order->set_payment_method_title($credit_card->method_title);
+    $order->save();
+    $order->calculate_totals();
 
-    // print_r($order->get_items());
+    $payment_processor = new VindiPaymentProcessor($order, $credit_card, $settings, $controllers);
+    $bill_items = $payment_processor->build_product_items('bill', $order->get_items());
 
-    // $payment_processor = new VindiPaymentProcessor($order, $credit_card, $settings, $controllers);
-    // $bill_items = $payment_processor->build_product_items('bill', $order->get_items());
-
-    // $this->assertEquals(
-    //   array(
-    //     array(
-    //       'product_id' => 0,
-    //       'quantity' => 4,
-    //       'pricing_schema' => array(
-    //         'price' => 10,
-    //         'schema_type' => 'per_unit'
-    //       )
-    //     ),
-    //     array(
-    //       'product_id' => null,
-    //       'amount' => 10
-    //     )
-    //   ),
-    //   $bill_items
-    // );
+    $this->assertEquals(
+      array(
+        array(
+          'product_id' => 1239812039,
+          'quantity' => 4,
+          'pricing_schema' => array(
+            'price' => 20.0,
+            'schema_type' => 'per_unit'
+          )
+        ),
+        array(
+          'product_id' => 465,
+          'amount' => 10.0
+        )
+      ),
+      $bill_items
+    );
   }
 }; ?>
