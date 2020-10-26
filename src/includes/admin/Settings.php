2eef2989fbdb49d39f15781834dc95bc6b216580
Laerte Guimarães <31661772+laerte-guimaraes@users.noreply.github.com>
2020-10-26 12:35:12 -0300

    Update Settings.php

--- src/includes/admin/Settings.php
+++ src/includes/admin/Settings.php
@@ -257,6 +257,7 @@ class VindiSettings extends WC_Settings_API
   {
     $api_key = $this->get_api_key();
     $this->api = new VindiApi($api_key, $this->logger, $this->get_is_active_sandbox());
+
     if (!$api_key) {
       return;
     }
@@ -278,10 +279,8 @@ class VindiSettings extends WC_Settings_API
   public function is_api_key_valid()
   {
     if($this->invalidApiKey) {
-      $this->logger->log('[warning] A Key é inválida');
       include_once VINDI_SRC . 'views/invalid-api-key.php';
     }
-    $this->logger->log('[info] A Key é válida');
   }
 
   /**
