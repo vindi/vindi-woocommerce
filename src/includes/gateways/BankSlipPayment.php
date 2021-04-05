4dd1f16e457e5eb9bba6037e0b3899efbbbd92d5
victorsinged! <victor.brito@vindi.com.br>
2021-04-05 13:48:08 -0300

    indentação

--- src/includes/gateways/BankSlipPayment.php
+++ src/includes/gateways/BankSlipPayment.php
@@ -97,10 +97,10 @@ class VindiBankSlipGateway extends VindiPaymentGateway
   public function bank_slip_quantity_to_render($order)
   {
       if (is_null($order[0])) {
-          return $order;
+            return $order;
       }
 
-      return $order[0];
+        return $order[0];
   }
 
   public function payment_fields()
@@ -129,9 +129,9 @@ class VindiBankSlipGateway extends VindiPaymentGateway
   {
       $order = wc_get_order($order_id);
       if ($order->get_payment_method() == 'vindi-bank-slip') {
-          $vindi_order = get_post_meta($order_id, 'vindi_order', true);
-          $order_to_iterate = $this->bank_slip_quantity_to_render($vindi_order);
-          $this->vindi_settings->get_template('bankslip-download.html.php', compact('vindi_order', 'order_to_iterate'));
+            $vindi_order = get_post_meta($order_id, 'vindi_order', true);
+            $order_to_iterate = $this->bank_slip_quantity_to_render($vindi_order);
+            $this->vindi_settings->get_template('bankslip-download.html.php', compact('vindi_order', 'order_to_iterate'));
       }
   }
 
