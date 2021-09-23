51456ac6c5529210147948deb7e4e4a9e094739e
Ana Catharina Mesquita <catha.ana.1994@gmail.com>
2021-09-23 18:40:42 -0300

    Escapando se não houver formulário para o cartão de crédito (#99)
    * fix: escapando se não houver formulário para o cartão de crédito
    
    * Versionamento 1.1.8
    
    Co-authored-by: laerte.guimaraes <laerte.guimaraes@live.com>

--- src/assets/js/frontend.js
+++ src/assets/js/frontend.js
@@ -1,6 +1,8 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
   'use strict';
-  const vindi_cc_input = function() {
+  const vindi_cc_input = function () {
+    if (!document.getElementById('vindi_cc_form-container')) return;
+
     const name = document.getElementById('vindi_cc_name');
     const cardnumber = document.getElementById('vindi_cc_cardnumber');
     const expirationdate = document.getElementById('vindi_cc_expirationdate');
@@ -17,7 +19,7 @@ jQuery(document).ready(function($) {
       mask: [
         {
           mask: '0000 0000 0000 0000',
-          regex:'^(4011(78|79)|43(1274|8935)|45(1416|7393|763(1|2))|50(4175|6699|67[0-7][0-9]|9000)|50(9[0-9][0-9][0-9])|627780|63(6297|6368)|650(03([^4])|04([0-9])|05(0|1)|05([7-9])|06([0-9])|07([0-9])|08([0-9])|4([0-3][0-9]|8[5-9]|9[0-9])|5([0-9][0-9]|3[0-8])|9([0-6][0-9]|7[0-8])|7([0-2][0-9])|541|700|720|727|901)|65165([2-9])|6516([6-7][0-9])|65500([0-9])|6550([0-5][0-9])|655021|65505([6-7])|6516([8-9][0-9])|65170([0-4]))\\d{0,15}',
+          regex: '^(4011(78|79)|43(1274|8935)|45(1416|7393|763(1|2))|50(4175|6699|67[0-7][0-9]|9000)|50(9[0-9][0-9][0-9])|627780|63(6297|6368)|650(03([^4])|04([0-9])|05(0|1)|05([7-9])|06([0-9])|07([0-9])|08([0-9])|4([0-3][0-9]|8[5-9]|9[0-9])|5([0-9][0-9]|3[0-8])|9([0-6][0-9]|7[0-8])|7([0-2][0-9])|541|700|720|727|901)|65165([2-9])|6516([6-7][0-9])|65500([0-9])|6550([0-5][0-9])|655021|65505([6-7])|6516([8-9][0-9])|65170([0-4]))\\d{0,15}',
           cardtype: 'elo'
         },
         {
@@ -86,7 +88,7 @@ jQuery(document).ready(function($) {
           from: 0,
           to: 99
         },
-        MM:  {
+        MM: {
           mask: IMask.MaskedRange,
           from: 1,
           to: 12
@@ -130,7 +132,7 @@ jQuery(document).ready(function($) {
     //pop in the appropriate card icon when detected
     cardnumber_mask.on("accept", function () {
       let card_type = cardnumber_mask.masked.currentMask.cardtype;
-      
+
       switch (card_type) {
         case 'elo':
           ccicon.innerHTML = elo;
@@ -267,13 +269,13 @@ jQuery(document).ready(function($) {
     });
   }
 
-  jQuery(window).load(function() {
+  jQuery(window).load(function () {
     vindi_cc_input();
   });
   jQuery('.vindi-old-cc-data').hide();
-  jQuery('body').on('updated_checkout', function() {
+  jQuery('body').on('updated_checkout', function () {
     vindi_cc_input();
-    jQuery(".vindi-change-card").bind('click', function(){
+    jQuery(".vindi-change-card").bind('click', function () {
       jQuery('.vindi-old-cc-data').hide();
       jQuery('.vindi-old-cc-data').find('input').prop('disabled', true);
 
@@ -283,7 +285,7 @@ jQuery(document).ready(function($) {
       return false;
     });
 
-    if(jQuery('.vindi-old-cc-data').length) {
+    if (jQuery('.vindi-old-cc-data').length) {
       jQuery('.vindi-old-cc-data').show();
       jQuery('.vindi-old-cc-data').find('input').prop('disabled', false);
 
