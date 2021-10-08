fe3b4071dae7f70b12d8a6b4cbd3252ccf85c67c
ThiagoDFi <57951485+ThiagoDFi@users.noreply.github.com>
2021-10-08 11:54:43 -0300

    adiciona retrocompatibilidade de cancelamento no plugin

--- src/utils/SubscriptionStatusHandler.php
+++ src/utils/SubscriptionStatusHandler.php
@@ -100,7 +100,10 @@ class VindiSubscriptionStatusHandler
         $subscription_id = method_exists($wc_subscription, 'get_id')
         ? $wc_subscription->get_id()
         : $wc_subscription->id;
-        return get_post_meta($subscription_id, 'vindi_subscription_id', true);
+        $new_subscription_id = get_post_meta($subscription_id, 'vindi_subscription_id', true);
+        $old_subscription_id = get_post_meta($subscription_id, 'vindi_wc_subscription_id', true);
+
+        return !empty($new_subscription_id) ? $new_subscription_id : $old_subscription_id;
     }
 
     /**
