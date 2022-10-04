3992ebf3bdc0b4a4b772a59eeef7e0f037cb15ec
Matheus Aguiar <aguiartgv@gmail.com>
2022-10-04 09:00:21 -0300

    fix: alterando mensagem de texto exibida. Antes solicita a desmarcação do campo, ao inverter a lógica devemos agora marcar o campo

--- src/templates/wcs-automatic-payment-deactivated-message.html.php
+++ src/templates/wcs-automatic-payment-deactivated-message.html.php
@@ -6,6 +6,6 @@
     </h3>
     <p>
         O plugin Vindi agora utiliza os mecanismos do WooCommerce para gerenciar as renovações das assinaturas.<br>
-        Para isso, você precisa desmarcar a opção "<i><?php echo __('Turn off Automatic Payments', 'woocommerce-subscriptions'); ?></i>" nas <a href="admin.php?page=wc-settings&tab=subscriptions">configurações do WooCommerce Subscriptions</a>.
+        Para isso, você precisa marcar a opção "<i><?php echo __('Turn off Automatic Payments', 'woocommerce-subscriptions'); ?></i>" nas <a href="admin.php?page=wc-settings&tab=subscriptions">configurações do WooCommerce Subscriptions</a>.
     </p>
 </div>
