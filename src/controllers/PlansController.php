4db4b6c44fa536bd9eefa98e8d8d445777901c55
Matheus Aguiar <aguiartgv@gmail.com>
2023-04-03 15:21:44 -0300

    fix: codeclimate issues

--- src/controllers/PlansController.php
+++ src/controllers/PlansController.php
@@ -38,6 +38,8 @@ class PlansController
    *
    * @since 1.2.2
    * @version 1.2.0
+   * 
+   * @SuppressWarnings(PHPMD.MissingImport)
    */
   function create($post_id, $post, $update, $recreated = false)
   {
--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -44,6 +44,8 @@ class ProductController
    *
    * @since 1.2.2
    * @version 1.2.0
+   * 
+   * @SuppressWarnings(PHPMD.MissingImport)
    */
   function create($post_id, $post, $update, $recreated = false)
   {
