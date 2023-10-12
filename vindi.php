8ecf725b91857dca58e14e87668884c02767b003
Matheus Aguiar <aguiartgv@gmail.com>
2023-10-12 08:24:07 -0300

    docs: atualizando arquivos de documentação do plugin

--- vindi.php
+++ vindi.php
@@ -6,7 +6,7 @@
  * Description: Adiciona o gateway de pagamento da Vindi para o WooCommerce.
  * Author: Vindi
  * Author URI: https://www.vindi.com.br
- * Version: 1.2.4
+ * Version: 1.2.5
  * Requires at least: 4.4
  * Tested up to: 6.3
  * WC requires at least: 3.0.0
