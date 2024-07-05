7e84b5d581aeeab1db3e962fbd4b8c587805fed3
Lucas Gama <lucas_tgama@hotmail.com>
2024-07-05 11:10:29 -0300

    fix: Versão atualizada de 1.3.2 para 1.3.3

--- vindi.php
+++ vindi.php
@@ -6,7 +6,7 @@
  * Description: Adiciona o gateway de pagamento da Vindi para o WooCommerce.
  * Author: Vindi
  * Author URI: https://www.vindi.com.br
- * Version: 1.3.2
+ * Version: 1.3.3
  * Requires at least: 4.4
  * Tested up to: 6.4
  * Text Domain: vindi-payment-gateway
