44533c2fd073bfb1065397cea98872ddf50fd04f
Lucas Gama <lucas_tgama@hotmail.com>
2024-09-16 15:33:19 -0300

    fix: comentando o js que gera o botão gerar link

--- src/utils/FrontendFilesLoader.php
+++ src/utils/FrontendFilesLoader.php
@@ -27,7 +27,7 @@ class FrontendFilesLoader {
     wp_register_style('vindi_woocommerce_admin_style', plugins_url('/assets/css/admin.css', plugin_dir_path(__FILE__)), array(), VINDI_VERSION);
     wp_enqueue_style('vindi_woocommerce_admin_style');
         wp_enqueue_script("vindi_products", plugins_url('/assets/js/product.js', plugin_dir_path(__FILE__)));
-        wp_register_script('vindi_woocommerce_edit_js', $dir_path, array('jquery'), VINDI_VERSION, true);
+        // wp_register_script('vindi_woocommerce_edit_js', $dir_path, array('jquery'), VINDI_VERSION, true);
         wp_enqueue_script('vindi_woocommerce_edit_js');
   }
     public static function frontendFiles()
